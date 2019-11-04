c801e7997e9a9b7168103cdec7c9474d89c21c87
young <y627756022@163.com>
2019-11-04 15:30:09 +0800

    update hystrix dashboard

--- blogyg-server-feign/src/main/java/com/blogyg/cloud/feign/BlogygServerFeignApplication.java
+++ blogyg-server-feign/src/main/java/com/blogyg/cloud/feign/BlogygServerFeignApplication.java
@@ -3,11 +3,13 @@ package com.blogyg.cloud.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrixDashboard
 public class BlogygServerFeignApplication {
 
     public static void main(String[] args) {
