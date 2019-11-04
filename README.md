a41318ea2426545a2262744191d79df12c9d84b6
young <y627756022@163.com>
2019-11-04 15:09:11 +0800

    update start application config and update readme.md desc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# spring-cloud-blogyg
-spring cloud demo
+# spring-cloud-demo
+
+### blogyg-server-eureka
+> 服务注册及发现模块
+### blogyg-server-client
+> 服务提供者
+### blogyg-server-ribbon | blogyg-server-feign
+> 服务消费者/负载均衡
--- blogyg-server-client/src/main/java/com/blogyg/cloud/client/BlogygServerClientApplication.java
+++ blogyg-server-client/src/main/java/com/blogyg/cloud/client/BlogygServerClientApplication.java
@@ -1,21 +1,15 @@
 package com.blogyg.cloud.client;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-import java.util.Scanner;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class BlogygServerClientApplication {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String port = scanner.nextLine();
-        new SpringApplicationBuilder(BlogygServerClientApplication.class)
-                .properties("server.port=" + port).run(args);
-//        SpringApplication.run(BlogygServerClientApplication.class, args);
+        SpringApplication.run(BlogygServerClientApplication.class, args);
     }
 
 }
