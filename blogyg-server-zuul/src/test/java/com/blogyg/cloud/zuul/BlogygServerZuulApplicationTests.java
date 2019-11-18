b2928bf494dc133027511967e919ad2b87d8594c
young <y627756022@163.com>
2019-11-18 18:02:40 +0800

    update zuul

---
+++ blogyg-server-zuul/src/test/java/com/blogyg/cloud/zuul/BlogygServerZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogyg.cloud.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogygServerZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
