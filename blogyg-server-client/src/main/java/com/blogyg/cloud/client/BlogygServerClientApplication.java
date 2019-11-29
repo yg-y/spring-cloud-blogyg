e6e768d5a55b6e4b28b7245767ddd9db2945b065
young <y627756022@163.com>
2019-11-29 16:44:19 +0800

    初步提交spring boot admin 监控 spring cloud服务 1.待完善自定义日志输出级别

--- blogyg-server-client/src/main/java/com/blogyg/cloud/client/BlogygServerClientApplication.java
+++ blogyg-server-client/src/main/java/com/blogyg/cloud/client/BlogygServerClientApplication.java
@@ -2,8 +2,10 @@ package com.blogyg.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 public class BlogygServerClientApplication {
--- blogyg-server-eureka/src/main/java/com/blogyg/cloud/eureka/BlogygServerEurekaApplication.java
+++ blogyg-server-eureka/src/main/java/com/blogyg/cloud/eureka/BlogygServerEurekaApplication.java
@@ -2,8 +2,10 @@ package com.blogyg.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class BlogygServerEurekaApplication {
@@ -13,3 +15,4 @@ public class BlogygServerEurekaApplication {
     }
 
 }
+
