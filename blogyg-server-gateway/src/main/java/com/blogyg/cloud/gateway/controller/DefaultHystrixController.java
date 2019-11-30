7817fa0b3a0574c02a0d47c0db67d7bdbf27ace3
young <y627756022@163.com>
2019-11-30 16:42:50 +0800

    更新 spring-boot-admin 实时监控微服务日志

--- blogyg-server-gateway/src/main/java/com/blogyg/cloud/gateway/controller/DefaultHystrixController.java
+++ blogyg-server-gateway/src/main/java/com/blogyg/cloud/gateway/controller/DefaultHystrixController.java
@@ -1,5 +1,6 @@
 package com.blogyg.cloud.gateway.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,13 @@ import java.util.Map;
 /**
  * 默认降级处理
  */
+@Slf4j
 @RestController
 public class DefaultHystrixController {
 
     @RequestMapping("/defaultfallback")
     public Map<String, String> defaultfallback() {
-        System.out.println("降级操作...");
+        log.info("降级操作...");
         Map<String, String> map = new HashMap<>();
         map.put("resultCode", "fail");
         map.put("resultMessage", "服务异常");
