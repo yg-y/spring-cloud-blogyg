80e462686bea3d27a6d1708b4986e5593168cc4f
young <young.yg@foxmail.com>
2023-02-09 10:48:48 +0800

    theme: add spring cloud alibaba version: 1.0 description: learning new framework

---
+++ blogyg-alibaba-cloud/alibaba-cloud-client/src/main/java/com/young/cloud/AlibabaCloudClientApplication.java
@@ -0,0 +1,13 @@
+package com.young.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaCloudClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaCloudClientApplication.class, args);
+    }
+
+}
---
+++ blogyg-alibaba-cloud/alibaba-cloud-client/src/test/java/com/young/cloud/AlibabaCloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaCloudClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
